d0de91394c03d269112a38a864bdac7f8c9257fa
hablijack <christoph.habel@posteo.de>
2021-04-29 13:36:23 +0200

    cleanup

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ SHUTTER_BTN_GPIO = 27
 SETTING_NEXT_BTN_GPIO = 23
 SETTING_PREV_BTN_GPIO = 25
 
+MODE_NEXT_BTN_GPIO = 22
+MODE_PREV_BTN_GPIO = 24
+
 current_cam_fx_index = 0
+current_cam_mode = 0
 
 FX_LIST = ['none', 'sketch', 'gpen', 'pastel', 'watercolor', 'oilpaint', 'hatch',
-           'negative', 'colorswap', 'posterise', 'denoise', 'blur', 'film',
-           'washedout', 'emboss', 'cartoon', 'solarize']
+           'negative', 'colorswap', 'posterise', 'washedout', 'emboss', 'cartoon', 'solarize']
+CAM_MODE_LIST = ['PIC', 'GIF']
 
 
 def initializeGPIOs():
@@ -19,7 +23,8 @@ def initializeGPIOs():
     GPIO.setup(SHUTTER_BTN_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(SETTING_NEXT_BTN_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(SETTING_PREV_BTN_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
+    GPIO.setup(MODE_NEXT_BTN_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(MODE_PREV_BTN_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def load_fx_mode(n):
     global current_cam_fx_index
@@ -27,34 +32,43 @@ def load_fx_mode(n):
     fx = FX_LIST[current_cam_fx_index]
     return fx
 
+def generate_overlay_text(effect):
+    overlay_text = None
+    if effect == 'none':
+        overlay_text = ' 4k '
+    else:
+        overlay_text = ' ' + effect + ' '
+    return overlay_text
 
 if __name__ == "__main__":
     initializeGPIOs()
     with PiCamera() as camera:
         camera.led = False
-        camera.resolution = (1920, 1080)
+        camera.resolution = (3840, 2160)
         camera.framerate = 30
-        frame = int(time.time())
         camera.annotate_text_size = 120
         camera.annotate_foreground = Color('black')
         camera.annotate_background = Color('white')
-        camera.annotate_text = ' ' + load_fx_mode(0) + ' '
+        camera.annotate_text = generate_overlay_text(load_fx_mode(0))
         camera.start_preview()
         while True:
             shutter_btn_state = GPIO.input(SHUTTER_BTN_GPIO)
             setting_next_btn_state = GPIO.input(SETTING_NEXT_BTN_GPIO)
             setting_prev_btn_state = GPIO.input(SETTING_PREV_BTN_GPIO)
+            mode_next_btn_state = GPIO.input(MODE_NEXT_BTN_GPIO)
+            mode_prev_btn_state = GPIO.input(MODE_PREV_BTN_GPIO)
             if setting_next_btn_state == False:
                 fx = load_fx_mode(1)
                 camera.image_effect = fx
-                camera.annotate_text = ' ' + fx + ' '
+                camera.annotate_text = generate_overlay_text(fx)
+                time.sleep(0.2)
             elif setting_prev_btn_state == False:
                 fx = load_fx_mode(-1)
                 camera.image_effect = fx
-                camera.annotate_text = ' ' + fx + ' '
+                camera.annotate_text = generate_overlay_text(fx)
+                time.sleep(0.2)
             elif shutter_btn_state == False:
                 camera.annotate_text = None
-                camera.capture('/home/pi/Pictures/%03d.jpg' % frame)
-                frame += 1
-                camera.annotate_text = ' ' + load_fx_mode(0) + ' '
-            time.sleep(0.2)
+                camera.capture('/home/pi/Pictures/%03d.jpg' % int(time.time_ns()))
+                camera.annotate_text = generate_overlay_text(load_fx_mode(0))
+                time.sleep(0.2)
--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-/usr/bin/sleep 10
-/usr/bin/python3 /home/pi/raspicam/start.sh
