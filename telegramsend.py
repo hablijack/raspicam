c5a39a46e7f6fe7e8916539f7b487d76c8c1e63a
hablijack <christoph.habel@posteo.de>
2021-05-04 19:42:26 +0200

    separate telegram job

--- telegramsend.py
+++ telegramsend.py
@@ -1,19 +1,32 @@
 from os import system, environ
 from telegram import Bot
+import os
 
 
-def send_picture_via_telegram_and_cleanup(bot, chat_id):
-    bot.send_photo(chat_id, photo=open('/home/pi/Pictures/image.jpg', 'rb'))
-    system("rm /home/pi/Pictures/image.jpg")
+def send_picture_via_telegram_and_cleanup(bot, chat_id, filename):
+    bot.send_photo(chat_id, photo=open(
+        '/home/pi/Pictures/' + filename + '.jpg', 'rb'))
+    system("rm /home/pi/Pictures/" + filename + ".jpg")
 
 
-def send_gif_via_telegram_and_cleanup(bot, chat_id):
+def send_gif_via_telegram_and_cleanup(bot, chat_id, filename):
     bot.send_animation(chat_id, animation=open(
-        '/home/pi/Pictures/gif/animation.gif', 'rb'))
-    system("rm /home/pi/Pictures/gif/animation.gif")
+        '/home/pi/Pictures/gif/' + filename + '.gif', 'rb'))
+    system('rm /home/pi/Pictures/gif/' + filename + '.gif')
 
 
 if __name__ == "__main__":
     telegram_bot_id = environ['TELEGRAM_BOT_ID']
     telegram_chat_id = environ['TELEGRAM_CHAT_ID']
+
     bot = Bot(telegram_bot_id)
+    directory = os.fsencode('/home/pi/Pictures')
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".jpg"):
+            send_picture_via_telegram_and_cleanup(
+                bot, telegram_chat_id, filename)
+        elif filename.endswith(".gif"):
+            send_gif_via_telegram_and_cleanup(bot, telegram_chat_id, filename)
+        else:
+            continue
